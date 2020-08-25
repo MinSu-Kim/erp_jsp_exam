91bac907990f37e0b12e08312d56c3030ac74b5e
teacher <net94.teacher@gmail.com>
2020-08-25 16:44:26 +0900

    DTO추가 및 Employee 테이블 입사일 컬럼 추가

--- src/erp_jsp_exam/TestMain.java
+++ src/erp_jsp_exam/TestMain.java
@@ -3,6 +3,7 @@ package erp_jsp_exam;
 import java.sql.Connection;
 
 import erp_jsp_exam.ds.HikariCPJAVA;
+import erp_jsp_exam.dto.Title;
 
 public class TestMain {
 
@@ -12,6 +13,24 @@ public class TestMain {
         
         Connection con = HikariCPJAVA.getConnection();
         System.out.println(con);
+        
+        /*reflection */
+        
+        try {
+            Class<?>dtoClass = Class.forName("erp_jsp_exam.dto.Title");
+            Title title = (Title) dtoClass.newInstance();
+            title.setNo(1);
+            title.setName("사장");
+            System.out.println(title);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
+        
+        
     }
 
 }
